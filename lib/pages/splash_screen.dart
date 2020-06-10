07edc865030b5a0c8d8f7c44265a8da22b1b8645
TobiAdeniji94 <adenijitobi94@gmail.com>
2020-06-10 05:39:32 +0100

    Added the splash screen

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:recipe/pages/create_edit_recipe.dart';
+import 'package:recipe/pages/list_recipe.dart';
+
+
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState () => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget> [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.orange[400],
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(top:40.0),
+                height: 350.0,
+                width: 350.0,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                      'assets/images/kisspng-chef-drawing-cooking.png'
+                    ),
+                    fit: BoxFit.fill,
+                  ),
+                  shape: BoxShape.rectangle,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 80.0),
+                child: Text('Recipe',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Shojumaru',
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              SizedBox(height: 30.0),
+              Container(
+                padding: EdgeInsets.only(left: 65.0, right: 65.0 ),
+                child: RaisedButton(
+                  onPressed: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => ListRecipe()));
+                  },
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  padding: EdgeInsets.all(0.0),
+                  child: Ink(
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                          color:Colors.black,
+                        ),
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(8.0)
+                    ),
+                    child: Container(
+                      constraints: BoxConstraints(
+                          minWidth: 88.0,
+                          minHeight: 50.0
+                      ),
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Get Started',
+                        style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Source_Sans_Pro',
+                            color: Colors.white
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
